6e17404a417f183619c82a01eaa9d7c5eb9bdd7c
Zubair <114304065+ZubairDev1@users.noreply.github.com>
2025-09-30 06:34:57 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { FaGithub, FaInstagram } from 'react-icons/fa';
 import { FaXTwitter } from 'react-icons/fa6';
+import profilePic from '../assets/pro Pic.jpg';
 
 function Hero() {
   return (
